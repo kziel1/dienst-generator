53bd8c2a486c851a9df0faba81f3cef0d5172424
kziel1 <krzysztofzielinski862@gmail.com>
2018-05-04 18:15:51 +0200

    init

---
+++ src/main/java/com/example/dienstgenerator/DienstGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.example.dienstgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DienstGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DienstGeneratorApplication.class, args);
+	}
+}
