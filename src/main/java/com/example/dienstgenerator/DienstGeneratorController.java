325b2073c96924bfee7c3b7b11f8c8e28c198151
kziel1 <krzysztofzielinski862@gmail.com>
2018-05-06 19:48:59 +0200

    website

---
+++ src/main/java/com/example/dienstgenerator/DienstGeneratorController.java
@@ -0,0 +1,15 @@
+package com.example.dienstgenerator;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableAutoConfiguration
+public class DienstGeneratorController {
+
+//    @RequestMapping("/")
+//    @ResponseBody
+//    public String get() {
+//return "asd";
+//    }
+}
